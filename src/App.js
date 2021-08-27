3cb5122bc5b7ec1cea23456004987d851a214fb4
Haley Hicks <haleyhicks90@yahoo.com>
2021-08-27 14:25:29 -0400

    first commit/push

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css';
+
+const App = () => {
+    return (
+        <>
+            <h1>sah du</h1>
+            <p>what now?</p>
+        </>    
+    )
+}
+export default App;
